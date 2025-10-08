6b69c5a5dc7c22ff0a3da2947c9c73cd4ab0281b
Nguyễn Thị Phương Chi <23020787@vnu.edu.vn>
2025-10-08 10:17:52 +0700

    Update NormalBrick.java

--- src/Brick/NormalBrick.java
+++ src/Brick/NormalBrick.java
@@ -4,6 +4,8 @@ public class NormalBrick extends Brick {
 
     public NormalBrick(int x, int y, int width, int height) {
         super(x, y, width, height);
+        this.type = 1;
+        this.hitPoints=1;
     }
 
     @Override
