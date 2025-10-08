8f0dc9a26ae44148f3afda26ed81d207cf707e64
Nguyễn Thị Phương Chi <23020787@vnu.edu.vn>
2025-10-08 10:18:38 +0700

    Update StrongBrick.java

--- src/Brick/StrongBrick.java
+++ src/Brick/StrongBrick.java
@@ -3,6 +3,8 @@ package Brick;
 public class StrongBrick extends Brick {
     public StrongBrick(int x, int y, int width, int height) {
         super(x, y, width, height);
+        this.type = 2;
+        this.hitPoints = 2;
     }
 
     @Override
